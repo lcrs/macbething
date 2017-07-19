df2af68eec5ab981518be9ecc0c66ef886dd9968
Lewis Saunders <lewis.saunders@gmail.com>
2017-07-19 15:14:12 +0100

    big man refactor

--- macbething.py
+++ macbething.py
@@ -1,52 +1,66 @@
-# To get the modules we need:
+# To get the modules we need on CentOS 7:
 # sudo yum install numpy openexr openexr-devel gcc-c++
 # sudo easy_install -Z openexr
 #   (openexr egg ends up in /usr/lib/python2.7/site-packages)
 
 import OpenEXR, Imath, numpy
-samplesize = 0.43
 
-frontexr = OpenEXR.InputFile('t.exr')
-dw = frontexr.header()['dataWindow']
-w = dw.max.x - dw.min.x + 1
-h = dw.max.y - dw.min.y + 1
-ptfloat = Imath.PixelType(Imath.PixelType.FLOAT)
+def exr2arrays(exrpath):
+	exr = OpenEXR.InputFile(exrpath)
+	dw = exr.header()['dataWindow']
+	w = dw.max.x - dw.min.x + 1
+	h = dw.max.y - dw.min.y + 1
+	ptfloat = Imath.PixelType(Imath.PixelType.FLOAT)
+	r = numpy.fromstring(exr.channel('R', ptfloat), dtype=numpy.float32)
+	r.shape = (h, w)
+	g = numpy.fromstring(exr.channel('G', ptfloat), dtype=numpy.float32)
+	g.shape = (h, w)
+	b = numpy.fromstring(exr.channel('B', ptfloat), dtype=numpy.float32)
+	b.shape = (h, w)
+	return r, g, b
 
-frontr = numpy.fromstring(frontexr.channel('R', ptfloat), dtype=numpy.float32)
-frontr.shape = (h, w)
-frontg = numpy.fromstring(frontexr.channel('G', ptfloat), dtype=numpy.float32)
-frontg.shape = (h, w)
-frontb = numpy.fromstring(frontexr.channel('B', ptfloat), dtype=numpy.float32)
-frontb.shape = (h, w)
+def arrays2exr(r, g, b, exrpath):
+	rstr = r.astype(numpy.float32).tostring()
+	gstr = g.astype(numpy.float32).tostring()
+	bstr = b.astype(numpy.float32).tostring()
+	exr = OpenEXR.OutputFile(exrpath, OpenEXR.Header(r.shape[1], r.shape[0]))
+	exr.writePixels({'R':rstr, 'G':gstr, 'B':bstr})
+	exr.close()
 
-y, x = numpy.mgrid[:h, :w]
-x = numpy.clip(1.025 * x - 0.025 * (w/2.0), 0.0, w)
-y = numpy.clip(1.090 * y - 0.090 * (h/2.0), 0.0, h)
-patchw, patchh = w/6.0, h/4.0
-u, v = (x % patchw) / patchw, (y % patchh) / patchh
-t0, t1 = 0.5 - samplesize, 0.5 + samplesize
-patchmask = (u > t0) * (u < t1) * (v > t0) * (v < t1)
-patchx, patchy = numpy.floor(x/patchw), numpy.floor(y/patchh)
-patchids = patchy * 6 + patchx + 1
-patchids *= patchmask
-patchids = patchids.astype(numpy.int32)
+def patchids(w, h, samplesize, rows, cols, xsqueeze, ysqueeze):
+	y, x = numpy.mgrid[0:h, 0:w]
+	x = numpy.clip((1.0 + xsqueeze) * x - xsqueeze * (w/2.0), 0.0, w)
+	y = numpy.clip((1.0 + ysqueeze) * y - ysqueeze * (h/2.0), 0.0, h)
+	patchw, patchh = w/float(cols), h/float(rows)
+	u, v = (x % patchw) / patchw, (y % patchh) / patchh
+	t0, t1 = 0.5 - samplesize, 0.5 + samplesize
+	patchmask = (u > t0) * (u < t1) * (v > t0) * (v < t1)
+	patchx, patchy = numpy.floor(x/patchw), numpy.floor(y/patchh)
+	ids = patchy * cols + patchx + 1
+	ids *= patchmask
+	ids = ids.astype(numpy.int32)
+	return ids
 
-sampleaccums = numpy.bincount(patchids.ravel(), weights=frontr.ravel())
-samplecounts = numpy.bincount(patchids.ravel())
-sampleavgs = sampleaccums / samplecounts
-ravg = sampleavgs[patchids]
-sampleaccums = numpy.bincount(patchids.ravel(), weights=frontg.ravel())
-samplecounts = numpy.bincount(patchids.ravel())
-sampleavgs = sampleaccums / samplecounts
-gavg = sampleavgs[patchids]
-sampleaccums = numpy.bincount(patchids.ravel(), weights=frontb.ravel())
-samplecounts = numpy.bincount(patchids.ravel())
-sampleavgs = sampleaccums / samplecounts
-bavg = sampleavgs[patchids]
+def samplechart(r, g, b, ids):
+	racc = numpy.bincount(ids.ravel(), weights=r.ravel())
+	rcount = numpy.bincount(ids.ravel())
+	ravg = racc / rcount
+	gacc = numpy.bincount(ids.ravel(), weights=g.ravel())
+	gcount = numpy.bincount(ids.ravel())
+	gavg = gacc / gcount
+	bacc = numpy.bincount(ids.ravel(), weights=b.ravel())
+	bcount = numpy.bincount(ids.ravel())
+	bavg = bacc / bcount
+	return ravg, gavg, bavg
 
-resultrstr = ravg.astype(numpy.float32).tostring()
-resultgstr = gavg.astype(numpy.float32).tostring()
-resultbstr = bavg.astype(numpy.float32).tostring()
-resultexr = OpenEXR.OutputFile('u.exr', OpenEXR.Header(w, h))
-resultexr.writePixels({'R':resultrstr, 'G':resultgstr, 'B':resultbstr})
-resultexr.close()
+def makechart(sr, sg, sb, ids):
+	r = sr[ids]
+	g = sg[ids]
+	b = sb[ids]
+	return r, g, b
+
+frontr, frontg, frontb = exr2arrays('front.exr')
+ids = patchids(frontr.shape[1], frontr.shape[0], 0.2, 4, 6, 0.025, 0.095)
+sr, sg, sb = samplechart(frontr, frontg, frontb, ids)
+resultr, resultg, resultb = makechart(sr, sg, sb, ids)
+arrays2exr(resultr, resultg, resultb, 'result.exr')
